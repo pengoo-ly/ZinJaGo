0749141ac53206eba04b2a39702ff9ff290677ac
I-Suppose <nothuman707@gmail.com>
2026-01-15 13:30:58 +0800

    Chieken added

---
+++ Week1_Practical1/Customer/Chicken.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Week1_Practical1.Customer
+{
+    public partial class Chicken : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
