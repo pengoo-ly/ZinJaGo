cf14edcfd8aa5f87b55c09c82162225f1aa0b29b
jinxious-yue <limyue041605@gmail.com>
2026-02-05 10:48:53 +0800

    courier update 2

--- Week1_Practical1/CourierManagement.aspx
+++ Week1_Practical1/CourierManagement.aspx
@@ -2,4 +2,52 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h3>Courier Management</h3>
+    <p>Organising tracking, and controlling parcel deliveries from pickup to final destination</p>
+    <br />
+        <asp:Panel ID="pnlAdd" runat="server" CssClass="panel-style">
+
+                <asp:Label Text="Courier Name" runat="server" />
+                <asp:TextBox ID="txtName" runat="server" CssClass="textbox" /><br /><br />
+
+                <asp:Label Text="Contact Number" runat="server" />
+                <asp:TextBox ID="txtContact" runat="server" CssClass="textbox" /><br /><br />
+
+                <asp:Label Text="Email" runat="server" />
+                <asp:TextBox ID="txtEmail" runat="server" CssClass="textbox" /><br /><br />
+
+                <asp:Label Text="Tracking URL" runat="server" />
+                <asp:TextBox ID="txtURL" runat="server" CssClass="textbox" /><br /><br />
+
+                <asp:CheckBox ID="chkPartnered" runat="server" Text=" Partnered Courier" /><br /><br />
+
+                <asp:Button ID="btnAdd" runat="server"
+                    Text="Add Courier"
+                    CssClass="btn-add"
+                    OnClick="btnAdd_Click" />
+
+            </asp:Panel>
+        <br />
+
+        <asp:GridView ID="gvCourier" runat="server"
+                CssClass="gridview-style"
+                AutoGenerateColumns="False"
+                DataKeyNames="CourierID"
+                OnRowEditing="gvCourier_RowEditing"
+                OnRowCancelingEdit="gvCourier_RowCancelingEdit"
+                OnRowUpdating="gvCourier_RowUpdating"
+                OnRowDeleting="gvCourier_RowDeleting">
+
+                <Columns>
+                    <asp:BoundField DataField="CourierID" HeaderText="ID" ReadOnly="true" />
+                    <asp:BoundField DataField="CourierName" HeaderText="Courier Name" />
+                    <asp:BoundField DataField="ContactNumber" HeaderText="Contact" />
+                    <asp:BoundField DataField="Email" HeaderText="Email" />
+                    <asp:BoundField DataField="TrackingURL" HeaderText="Tracking URL" />
+                    <asp:CheckBoxField DataField="IsPartnered" HeaderText="Partnered" />
+
+                    <asp:CommandField ShowEditButton="true" ShowDeleteButton="true" />
+                </Columns>
+            <HeaderStyle CssClass="gv-header"></HeaderStyle>
+            </asp:GridView>
 </asp:Content>
--- Week1_Practical1/CourierManagement.aspx.cs
+++ Week1_Practical1/CourierManagement.aspx.cs
@@ -13,9 +13,101 @@ namespace Week1_Practical1
 {
     public partial class CourierManagement : System.Web.UI.Page
     {
+        Courier c = new Courier();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+                BindGrid();
+        }
+        void BindGrid()
+        {
+            gvCourier.DataSource = c.GetAllCouriers();
+            gvCourier.DataBind();
+        }
 
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Courier newCourier = new Courier();
+                newCourier.CourierID = newCourier.GetNextCourierID();
+                newCourier.CourierName = txtName.Text;
+                newCourier.ContactNumber = txtContact.Text;
+                newCourier.Email = txtEmail.Text;
+                newCourier.TrackingURL = txtURL.Text;
+                newCourier.IsPartnered = chkPartnered.Checked;
+
+                newCourier.InsertCourier();
+                BindGrid();
+            }
+            catch
+            {
+                Response.Write("<script>alert('Failed to add new courier.');</script>");
+            }
+        }
+
+        protected void gvCourier_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            try
+            {
+                gvCourier.EditIndex = e.NewEditIndex;
+                BindGrid();
+            }
+            catch 
+            {
+                Response.Write("<script>alert('Failed to enter edit mode.');</script>");
+            }
+        }
+
+        protected void gvCourier_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            try
+            {
+                gvCourier.EditIndex = -1;
+                BindGrid();
+            }
+            catch
+            {
+                Response.Write("<script>alert('Failed to cancel edit.');</script>");
+            }
+        }
+
+        protected void gvCourier_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                int id = Convert.ToInt32(gvCourier.DataKeys[e.RowIndex].Value);
+
+                Courier update = new Courier();
+                update.CourierID = id;
+                update.CourierName = ((TextBox)gvCourier.Rows[e.RowIndex].Cells[1].Controls[0]).Text;
+                update.ContactNumber = ((TextBox)gvCourier.Rows[e.RowIndex].Cells[2].Controls[0]).Text;
+                update.Email = ((TextBox)gvCourier.Rows[e.RowIndex].Cells[3].Controls[0]).Text;
+                update.TrackingURL = ((TextBox)gvCourier.Rows[e.RowIndex].Cells[4].Controls[0]).Text;
+                update.IsPartnered = ((CheckBox)gvCourier.Rows[e.RowIndex].Cells[5].Controls[0]).Checked;
+
+                update.UpdateCourier();
+                gvCourier.EditIndex = -1;
+                BindGrid();
+            }
+            catch 
+            {
+                Response.Write("<script>alert('Failed to update courier details.');</script>");
+            }
+        }
+
+        protected void gvCourier_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                int id = Convert.ToInt32(gvCourier.DataKeys[e.RowIndex].Value);
+                c.DeleteCourier(id);
+                BindGrid();
+            }
+            catch 
+            {
+                Response.Write("<script>alert('Failed to delete courier.');</script>");
+            }
         }
     }
 }
